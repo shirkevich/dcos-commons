48f35850918d12208006afac957557fce5b4be80
Sunil Shah <sunil@mesosphere.io>
2017-05-15 13:39:43 -0700

    Raw initial implementation of CLI talking to Cosmos.

--- cli/commands/service.go
+++ cli/commands/service.go
@@ -1,7 +1,7 @@
 package commands
 
 import (
-	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 
@@ -10,6 +10,7 @@ import (
 	"bytes"
 
 	"github.com/mesosphere/dcos-commons/cli/client"
+	"github.com/mesosphere/dcos-commons/cli/config"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
@@ -17,7 +18,11 @@ type DescribeHandler struct {
 	DescribeName string
 }
 
-func parseResolvedOptions(responseBytes []byte) string {
+type DescribeRequest struct {
+	AppID string `json:"appId"`
+}
+
+func unmarshallJSON(responseBytes []byte) map[string]interface{} {
 	var responseJSON map[string]interface{}
 	err := json.Unmarshal([]byte(responseBytes), &responseJSON)
 	if err != nil {
@@ -27,36 +32,71 @@ func parseResolvedOptions(responseBytes []byte) string {
 		outBuf.WriteTo(os.Stdout)
 		os.Exit(1)
 	}
-	var resolvedOptions = responseJSON["properties"].(map[string]interface{})["resolvedOptions"].(string)
-	return resolvedOptions
+	return responseJSON
+}
+
+func parseDescribeResponse(responseBytes []byte) string {
+	// TODO: what is the intended output here? Do we want to show upgradesTo/downgradesTo components?
+	responseJSON := unmarshallJSON(responseBytes)
+	return responseJSON["properties"].(map[string]interface{})["resolvedOptions"].(string)
 }
 
 func (cmd *DescribeHandler) DescribeConfiguration(c *kingpin.ParseContext) error {
-	// Call out to <dcos_url>/cosmos/service/describe w/ auth header
-	//      Accept:'application/vnd.dcos.service.describe-response+json;charset=utf-8;version=v1' \
-	//      Content-Type:'application/vnd.dcos.service.describe-request+json;charset=utf-8;version=v1' \
-	//      appId="$1"
-	// TODO: what is the intended output here? Do we want to show upgradesTo/downgradesTo components?
-	// TODO: set arguments
-	urlPath := "cosmos/service/describe"
-	response := client.HTTPGet(urlPath)
-	resolvedOptions := parseResolvedOptions(client.GetResponseBytes(response))
+	// TODO: add error handling
+	requestContent, _ := json.Marshal(DescribeRequest{config.ServiceName})
+	cosmosUrlPath := "service/describe"
+	response := client.HTTPCosmosGetJSON(cosmosUrlPath, string(requestContent))
+	resolvedOptions := parseDescribeResponse(client.GetResponseBytes(response))
 	client.PrintText(resolvedOptions)
 	return nil
 }
 
 type UpdateHandler struct {
 	UpdateName     string
-	File           *os.File
+	OptionsFile    string
 	PackageVersion string
 }
 
+type UpdateRequest struct {
+	AppID          string `json:"appId"`
+	PackageVersion string `json:"packageVersion"`
+	OptionsJSON    string `json:"options"`
+}
+
+func checkAndReadFile(filename string) (string, error) {
+	// TODO: any validation?
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return "", err
+	}
+	return string(data), nil
+}
+
+func parseUpdateResponse(responseBytes []byte) string {
+	// TODO: do something interesting with this output
+	// Output should be in the same format as the `dcos marathon app update` command
+	responseJSON := unmarshallJSON(responseBytes)
+	return responseJSON["properties"].(map[string]interface{})["marathonDeploymentId"].(string)
+}
+
 func (cmd *UpdateHandler) UpdateConfiguration(c *kingpin.ParseContext) error {
-	// Call out to <dcos_url>/cosmos/service/update w/ auth header
-	// Accept:'application/vnd.dcos.service.update-response+json;charset=utf-8;version=v1' \
-	// Content-Type:'application/vnd.dcos.service.update-request+json;charset=utf-8;version=v1' \
-	// TODO: read file in
-	fmt.Printf("Updatin")
+	cosmosUrlPath := "service/update"
+	request := UpdateRequest{AppID: config.ServiceName}
+	if len(cmd.PackageVersion) > 0 {
+		// TODO: check package version format is valid
+		request.PackageVersion = cmd.PackageVersion
+	}
+	if len(cmd.OptionsFile) > 0 {
+		optionsJSON, err := checkAndReadFile(cmd.OptionsFile)
+		if err != nil {
+			log.Fatalf("Failed to load specified options file %s: %s", cmd.OptionsFile, err)
+		}
+		request.OptionsJSON = optionsJSON
+	}
+	requestContent, _ := json.Marshal(request)
+	response := client.HTTPCosmosPostJSON(cosmosUrlPath, string(requestContent))
+	outputResponse := parseUpdateResponse(client.GetResponseBytes(response))
+	client.PrintText(outputResponse)
 	return nil
 }
 
@@ -68,6 +108,6 @@ func HandleServiceSection(app *kingpin.Application) {
 
 	updateCmd := &UpdateHandler{}
 	update := pkg.Command("update", "Update the package version or configuration for this DC/OS service").Action(updateCmd.UpdateConfiguration)
-	update.Flag("--options", "Path to a JSON file that contains customized package installation options").FileVar(&updateCmd.File)
+	update.Flag("--options", "Path to a JSON file that contains customized package installation options").StringVar(&updateCmd.OptionsFile)
 	update.Flag("--package-version", "The desired package version.").StringVar(&updateCmd.PackageVersion)
 }
