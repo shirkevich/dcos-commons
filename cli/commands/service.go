461ced59ffbfc57aa5c69d281f6c07931c3f7f85
Sunil Shah <sunil@mesosphere.io>
2017-05-16 13:27:43 -0700

    Working JSON parsing.

--- cli/commands/service.go
+++ cli/commands/service.go
@@ -1,13 +1,14 @@
 package commands
 
 import (
+	"bytes"
 	"io/ioutil"
 	"log"
 	"os"
 
 	"encoding/json"
 
-	"bytes"
+	"fmt"
 
 	"github.com/mesosphere/dcos-commons/cli/client"
 	"github.com/mesosphere/dcos-commons/cli/config"
@@ -22,31 +23,45 @@ type DescribeRequest struct {
 	AppID string `json:"appId"`
 }
 
-func unmarshallJSON(responseBytes []byte) map[string]interface{} {
+func unmarshallJSON(jsonBytes []byte) (map[string]interface{}, error) {
 	var responseJSON map[string]interface{}
-	err := json.Unmarshal([]byte(responseBytes), &responseJSON)
+	err := json.Unmarshal([]byte(jsonBytes), &responseJSON)
 	if err != nil {
-		log.Printf("Failed to unmarshall JSON from response.")
-		log.Printf("Original data follows:")
-		outBuf := *bytes.NewBuffer(responseBytes)
-		outBuf.WriteTo(os.Stdout)
-		os.Exit(1)
+		return nil, err
 	}
-	return responseJSON
+	return responseJSON, nil
 }
 
-func parseDescribeResponse(responseBytes []byte) string {
-	// TODO: what is the intended output here? Do we want to show upgradesTo/downgradesTo components?
-	responseJSON := unmarshallJSON(responseBytes)
-	return responseJSON["properties"].(map[string]interface{})["resolvedOptions"].(string)
+func parseDescribeResponse(responseBytes []byte) ([]byte, error) {
+	// TODO: what is the intended output here?
+	// Do we want to show upgradesTo/downgradesTo components? resolvedOptions or userProvidedOptions?
+	// TODO: is there a better way to do this instead of unmarshalling and remarshalling?
+	// TODO: add some error handling here in case the format changes
+	responseJSONBytes, err := unmarshallJSON(responseBytes)
+	if err != nil {
+		return nil, err
+	}
+	resolvedOptions, err := json.Marshal(responseJSONBytes["resolvedOptions"])
+	if err != nil {
+		return nil, err
+	}
+	return resolvedOptions, nil
 }
 
 func (cmd *DescribeHandler) DescribeConfiguration(c *kingpin.ParseContext) error {
 	// TODO: add error handling
 	requestContent, _ := json.Marshal(DescribeRequest{config.ServiceName})
-	response := client.HTTPCosmosGetJSON("describe", string(requestContent))
-	resolvedOptions := parseDescribeResponse(client.GetResponseBytes(response))
-	client.PrintText(resolvedOptions)
+	response := client.HTTPCosmosPostJSON("describe", string(requestContent))
+	responseBytes := client.GetResponseBytes(response)
+	resolvedOptions, err := parseDescribeResponse(responseBytes)
+	if err != nil {
+		log.Printf("Failed to unmarshall response.")
+		log.Printf("Original data follows:")
+		outBuf := *bytes.NewBuffer(responseBytes)
+		outBuf.WriteTo(os.Stdout)
+		os.Exit(1)
+	}
+	client.PrintJSONBytes(resolvedOptions, nil)
 	return nil
 }
 
@@ -57,25 +72,28 @@ type UpdateHandler struct {
 }
 
 type UpdateRequest struct {
-	AppID          string `json:"appId"`
-	PackageVersion string `json:"packageVersion"`
-	OptionsJSON    string `json:"options"`
+	AppID          string                 `json:"appId"`
+	PackageVersion string                 `json:"packageVersion,omitempty"`
+	OptionsJSON    map[string]interface{} `json:"options,omitempty"`
 }
 
-func checkAndReadFile(filename string) (string, error) {
+func checkAndReadJSONFile(filename string) (map[string]interface{}, error) {
 	// TODO: any validation?
-	data, err := ioutil.ReadFile(filename)
+	fileBytes, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return unmarshallJSON(fileBytes)
+}
+
+func parseUpdateResponse(responseBytes []byte) (string, error) {
+	// TODO: do something interesting with this output
+	// Output should be in the same format as the `dcos marathon app update` command
+	responseJSON, err := unmarshallJSON(responseBytes)
 	if err != nil {
 		return "", err
 	}
-	return string(data), nil
-}
-
-func parseUpdateResponse(responseBytes []byte) string {
-	// TODO: do something interesting with this output
-	// Output should be in the same format as the `dcos marathon app update` command
-	responseJSON := unmarshallJSON(responseBytes)
-	return responseJSON["properties"].(map[string]interface{})["marathonDeploymentId"].(string)
+	return string(responseJSON["marathonDeploymentId"].(string)), nil
 }
 
 func (cmd *UpdateHandler) UpdateConfiguration(c *kingpin.ParseContext) error {
@@ -86,7 +104,7 @@ func (cmd *UpdateHandler) UpdateConfiguration(c *kingpin.ParseContext) error {
 		request.PackageVersion = cmd.PackageVersion
 	}
 	if len(cmd.OptionsFile) > 0 {
-		optionsJSON, err := checkAndReadFile(cmd.OptionsFile)
+		optionsJSON, err := checkAndReadJSONFile(cmd.OptionsFile)
 		if err != nil {
 			log.Fatalf("Failed to load specified options file %s: %s", cmd.OptionsFile, err)
 		}
@@ -94,8 +112,16 @@ func (cmd *UpdateHandler) UpdateConfiguration(c *kingpin.ParseContext) error {
 	}
 	requestContent, _ := json.Marshal(request)
 	response := client.HTTPCosmosPostJSON("update", string(requestContent))
-	outputResponse := parseUpdateResponse(client.GetResponseBytes(response))
-	client.PrintText(outputResponse)
+	responseBytes := client.GetResponseBytes(response)
+	deploymentID, err := parseUpdateResponse(responseBytes)
+	if err != nil {
+		log.Printf("Failed to unmarshall response.")
+		log.Printf("Original data follows:")
+		outBuf := *bytes.NewBuffer(responseBytes)
+		outBuf.WriteTo(os.Stdout)
+		os.Exit(1)
+	}
+	client.PrintText(fmt.Sprintf("Created deployment %s", deploymentID))
 	return nil
 }
 
@@ -107,6 +133,6 @@ func HandleServiceSection(app *kingpin.Application) {
 
 	updateCmd := &UpdateHandler{}
 	update := pkg.Command("update", "Update the package version or configuration for this DC/OS service").Action(updateCmd.UpdateConfiguration)
-	update.Flag("--options", "Path to a JSON file that contains customized package installation options").StringVar(&updateCmd.OptionsFile)
-	update.Flag("--package-version", "The desired package version.").StringVar(&updateCmd.PackageVersion)
+	update.Flag("options", "Path to a JSON file that contains customized package installation options").StringVar(&updateCmd.OptionsFile)
+	update.Flag("package-version", "The desired package version.").StringVar(&updateCmd.PackageVersion)
 }
